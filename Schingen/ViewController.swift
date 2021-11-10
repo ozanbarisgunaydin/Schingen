40819b9b32a085839d1564718dd38bdc6490a60c
Ozan Barış GÜNAYDIN <ozangunaydin61@gmail.com>
2021-11-10 17:07:56 +0300

    Initial Commit

---
+++ Schingen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schingen
+//
+//  Created by Ozan Barış Günaydın on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
